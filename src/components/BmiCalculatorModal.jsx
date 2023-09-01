46c07f45e1019cca6aea55d4c56a791eac3d0c8f
Michał <mirossian@gmail.com>
2023-09-01 17:27:24 +0200

    Finished styling for modal and users list

--- src/components/BmiCalculatorModal.jsx
+++ src/components/BmiCalculatorModal.jsx
@@ -47,7 +47,7 @@ function BmiCalculatorModal({isVisible, handleModal, user}) {
                 </Transition.Child>
 
                 <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
@@ -64,66 +64,68 @@ function BmiCalculatorModal({isVisible, handleModal, user}) {
                                         <div className="mt-3 text-center sm:mt-0 sm:text-left">
                                             <Dialog.Title as="h3"
                                                           className="text-base font-semibold leading-6 text-gray-900">
-                                                BMI Calculator
+                                                Kalkulator BMI
                                             </Dialog.Title>
                                             <div className="mt-2">
                                                 <div className="container mx-auto p-4">
-                                                    <div className="grid grid-cols-2 gap-4">
+                                                    <div className="grid grid-cols-1 gap-4">
                                                         <div>
                                                             <label
-                                                                className="block text-gray-700 text-sm font-bold mb-2">Sex:</label>
+                                                                className="block text-gray-700 text-sm font-bold mb-2">Płeć:</label>
                                                             <div className="flex items-center ">
-                                                                <label className="flex mr-2 items-center">
+                                                                <label className="flex mr-5 items-center">
                                                                     <input
+                                                                        className='mr-2'
                                                                         type="radio"
                                                                         value="male"
                                                                         checked={sex === 'male'}
                                                                         onChange={() => setSex('male')}
                                                                     />
-                                                                    Male
+                                                                    Mężczyzna
                                                                 </label>
                                                                 <label className="flex items-center">
                                                                     <input
+                                                                        className='mr-2'
                                                                         type="radio"
                                                                         value="female"
                                                                         checked={sex === 'female'}
                                                                         onChange={() => setSex('female')}
                                                                     />
-                                                                    Female
+                                                                    Kobieta
                                                                 </label>
                                                             </div>
                                                         </div>
                                                         <div>
                                                             <label
-                                                                className="block text-gray-700 text-sm font-bold mb-2">Age:</label>
+                                                                className="block text-gray-700 text-sm font-bold mb-2">Wiek:</label>
                                                             <input
                                                                 type="number"
                                                                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                                                                placeholder="Age"
+                                                                placeholder="Wiek"
                                                                 value={age}
                                                                 onChange={(e) => setAge(e.target.value)}
                                                             />
                                                         </div>
                                                         <div>
                                                             <label
-                                                                className="block text-gray-700 text-sm font-bold mb-2">Height
+                                                                className="block text-gray-700 text-sm font-bold mb-2">Wzrost
                                                                 (cm):</label>
                                                             <input
                                                                 type="number"
                                                                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                                                                placeholder="Height"
+                                                                placeholder="Wzrost"
                                                                 value={height}
                                                                 onChange={(e) => setHeight(e.target.value)}
                                                             />
                                                         </div>
                                                         <div>
                                                             <label
-                                                                className="block text-gray-700 text-sm font-bold mb-2">Weight
+                                                                className="block text-gray-700 text-sm font-bold mb-2">Waga
                                                                 (kg):</label>
                                                             <input
                                                                 type="number"
                                                                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                                                                placeholder="Weight"
+                                                                placeholder="Waga"
                                                                 value={weight}
                                                                 onChange={(e) => setWeight(e.target.value)}
                                                             />
@@ -131,7 +133,7 @@ function BmiCalculatorModal({isVisible, handleModal, user}) {
                                                     </div>
                                                     {bmi && (
                                                         <div className="mt-4">
-                                                            <p className="font-bold">Your BMI: {bmi}</p>
+                                                            <p className="font-bold">Twoje BMI: {bmi}</p>
                                                         </div>
                                                     )}
                                                 </div>
@@ -139,21 +141,22 @@ function BmiCalculatorModal({isVisible, handleModal, user}) {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <div className="bg-gray-50 px-4 py-3 flex flex-col-reverse sm:flex-row-reverse sm:px-6">
                                     <button
                                         type="button"
-                                        className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                                        className="mb-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 sm:ml-3 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                        onClick={() => handleModal()}
+                                    >
+                                        Anuluj
+                                    </button>
+
+                                    <button
+                                        type="button"
+                                        className="mb-3 inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:w-auto"
                                         onClick={calculateBMI}
                                         ref={calculateButtonRef}
                                     >
-                                        Calculate BMI
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                        onClick={() => handleModal()}
-                                    >
-                                        Cancel
+                                        Oblicz BMI
                                     </button>
                                 </div>
                             </Dialog.Panel>
